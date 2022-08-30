6f8de239b3a71e5112270fa5d109f720b893782c
Fabricio Oliveira <fabriciodeoliveira334@gmail.com>
2022-08-29 21:35:16 -0300

    animação no name

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,9 @@ sr.reveal(".about", {
   duration: 2000,
 });
 sr.reveal(".project", { origin: "right", distance: "200px", duration: 2000 });
+
+sr.reveal(".name", {
+  origin: "top",
+  distance: "150px",
+  duration: 2000,
+});
